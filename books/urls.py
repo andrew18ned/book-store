8cbd1f94a0a19641f631f0bd0d1acacb1f8f3460
Andrew Nedilskej <andrej.nedilskej@gmail.com>
2022-05-22 08:40:40 +0300

    create auth for social. GitHub

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.db import router
-from django.urls import path
+from django.urls import path, include, re_path as url
 from rest_framework.routers import SimpleRouter
-from store.views import BookViewSet
+from store.views import BookViewSet, auth
 
 
 router = SimpleRouter()
@@ -10,6 +10,8 @@ router.register(r'book', BookViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url('', include('social_django.urls', namespace='social')),
+    path('auth/', auth),
 ]
 
 urlpatterns += router.urls
